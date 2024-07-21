37c259f0e2ffdfa3bec22aba50d37b38a0b5f69f
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2024-07-21 19:06:38 +0300

    Console test

---
+++ Ev3ConsoleTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using EV3DecompilerLib.Decompile;
+using EV3DecompilerLib.Recognize;
+using EV3ModelLib;
+
+namespace Ev3ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"./TestPrograms/Program.rbf";
+            var data = File.ReadAllBytes(path);
+            string text = GetText(data, path);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+
+        private static string GetText(byte[] data, string path)
+        {
+            var bytecode = new Decompiler().BuildByteCodeForData(data, Path.GetFileName(path));
+            var recognizedEV3Blocks = new PatternMatcher().RecognizeEV3Calls(bytecode, Path.GetFileName(path));
+            PrintOptionsClass poc = new PrintOptionsClass()
+            {
+                TargetColorIndex = PrintOptionsClass.TargetColorEnum.None,
+            };
+            Node.PrintOptions = poc;
+            var nd = NodeConversion.Convert(recognizedEV3Blocks.program);
+
+            return PrintHelper.GetPMDListString(nd);
+        }
+    }
+}
