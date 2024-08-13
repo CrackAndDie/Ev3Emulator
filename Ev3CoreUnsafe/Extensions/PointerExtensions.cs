cf07292df9068419825166259378adf5118704b7
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2024-08-13 19:31:18 +0300

    restored memory leaks warnings

--- Ev3CoreUnsafe/Extensions/PointerExtensions.cs
+++ Ev3CoreUnsafe/Extensions/PointerExtensions.cs
@@ -2,10 +2,9 @@
 
 namespace Ev3CoreUnsafe.Extensions
 {
-    [Obsolete("Possible memory leaks")]
     public static class PointerExtensions
 	{
-		// [Obsolete("Possible memory leaks")]
+		[Obsolete("Possible memory leaks")]
 		public unsafe static T* AsPointer<T>(this T[] buf) where T: unmanaged
 		{
 			var p = (T*)Unsafe.AsPointer(ref GC.AllocateArray<T>(buf.Length, true)[0]);
@@ -16,8 +15,8 @@ namespace Ev3CoreUnsafe.Extensions
 			return p;
 		}
 
-		
-		public unsafe static DATA8* AsSbytePointer(this string str)
+        [Obsolete("Possible memory leaks")]
+        public unsafe static DATA8* AsSbytePointer(this string str)
 		{
 			var p = (DATA8*)Unsafe.AsPointer(ref GC.AllocateArray<DATA8>(str.Length + 1, true)[0]);
 			for (int i = 0; i < str.Length; ++i)
