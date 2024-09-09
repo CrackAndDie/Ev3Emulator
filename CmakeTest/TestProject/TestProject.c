7f0b32a7ee68da39a0a6dc91198a47628b73d8d0
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2024-09-09 09:33:38 +0300

    Andrei stuff :)

--- CmakeTest/TestProject/TestProject.c
+++ CmakeTest/TestProject/TestProject.c
@@ -2,14 +2,27 @@
 //
 
 #include "TestProject.h"
+#include <stdlib.h>
 
-static int anime = 42;
-
-void getAbb(int* abb) {
-	*abb = sizeof(int*);
-	anime = *abb;
+unsigned char* custom_malloc(unsigned int amount, unsigned char fillZeros) {
+	unsigned char* data;
+	data = (unsigned char*)malloc(amount);
+	if (fillZeros && data != NULL) {
+		for (int i = 0; i < amount; ++i) {
+			data[i] = 0;
+		}
+	}
+	return data;
 }
 
-int getAnime() {
-	return anime;
+void custom_free(unsigned char* ptr) {
+	free(ptr);
+}
+
+int anime322(unsigned int point_num_per_ch, short* p_raw_data, unsigned int* p_points_per_ch_returned) {
+	for (unsigned int i = 0; i < point_num_per_ch; ++i) {
+		p_raw_data[i] = 1488;
+	}
+	*p_points_per_ch_returned = point_num_per_ch;
+	return 0;
 }
--- CmakeTest/TestProject/TestProject.h
+++ CmakeTest/TestProject/TestProject.h
@@ -4,9 +4,9 @@
 #ifndef   TESTPROJECT_H_
 #define   TESTPROJECT_H_
 
-__declspec(dllexport) void getAbb(int* abb);
-__declspec(dllexport) int getAnime();
+__declspec(dllexport) unsigned char* custom_malloc(unsigned int amount, unsigned char fillZeros);
+__declspec(dllexport) void custom_free(unsigned char* ptr);
 
-// TODO: Reference additional headers your program requires here.
+__declspec(dllexport) int anime322(unsigned int point_num_per_ch, short* p_raw_data, unsigned int* p_points_per_ch_returned);
 
 #endif /* TESTPROJECT_H_ */
